554fe908f467a9cdc7b9826ebe0261b9077d0ebb
= <=>
2021-03-08 05:44:44 +0700

    polinema_mobile_flutter_hello

--- lib/main.dart
+++ lib/main.dart
@@ -46,8 +46,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -55,7 +53,6 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
     });
   }
 
